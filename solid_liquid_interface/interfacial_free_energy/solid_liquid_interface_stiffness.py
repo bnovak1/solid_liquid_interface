b0ef61e2bf7d41094931058bb7f8343025a204c6
Brian Novak <brian.novak.simulation@gmail.com>
2020-02-13 19:40:00 -0600

    Removed pasteurize imports

--- solid_liquid_interface/interfacial_free_energy/solid_liquid_interface_stiffness.py
+++ solid_liquid_interface/interfacial_free_energy/solid_liquid_interface_stiffness.py
@@ -1,16 +1,3 @@
-# added by pasteurize
-#########################################################################################
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import absolute_import
-from builtins import open
-from builtins import int
-from builtins import range
-from future import standard_library
-standard_library.install_aliases()
-#########################################################################################
-
 import mdtraj
 from joblib import Parallel, delayed, cpu_count
 import numpy as np
